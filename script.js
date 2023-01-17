e8c4add641d34123ece2adbc1adbc380afbad556
Justus Kelsch <33078472+JustusKelsch@users.noreply.github.com>
2023-01-16 21:39:34 -0700

    Added logic for each basic math operator. Added logic to determine which operator function to call. Added number buttons and operator buttons. Added functionality to update the screen when a number button is pressed

---
+++ script.js
@@ -0,0 +1,41 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    if (operator === "+") {
+        return add(a, b);
+    }
+    else if (operator === "-") {
+        return subtract(a, b);
+    }
+    else if (operator === "*") {
+        return multiply(a, b);
+    }
+    else if (operator === "/") {
+        return divide(a, b);
+    }
+}
+
+let screen = document.querySelector('.screen');
+
+function updateScreen(item) {
+    screen.innerHTML += item;
+}
+
+const buttons = document.querySelectorAll('.nums');
+
+buttons.forEach(button => {button.addEventListener('click', function() {updateScreen(button.innerHTML)})});
+console.log(buttons);
